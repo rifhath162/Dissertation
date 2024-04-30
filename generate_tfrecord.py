5c20bf59528c7a6321f8836a2c2d98c0531edf7b
rifhath162 <147535738+rifhath162@users.noreply.github.com>
2024-04-30 01:08:49 +0100

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,21 +1,3 @@
-""" Sample TensorFlow XML-to-TFRecord converter
-
-usage: generate_tfrecord.py [-h] [-x XML_DIR] [-l LABELS_PATH] [-o OUTPUT_PATH] [-i IMAGE_DIR] [-c CSV_PATH]
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -x XML_DIR, --xml_dir XML_DIR
-                        Path to the folder where the input .xml files are stored.
-  -l LABELS_PATH, --labels_path LABELS_PATH
-                        Path to the labels (.pbtxt) file.
-  -o OUTPUT_PATH, --output_path OUTPUT_PATH
-                        Path of output TFRecord (.record) file.
-  -i IMAGE_DIR, --image_dir IMAGE_DIR
-                        Path to the folder where the input image files are stored. Defaults to the same directory as XML_DIR.
-  -c CSV_PATH, --csv_path CSV_PATH
-                        Path of output .csv file. If none provided, then no file will be written.
-"""
-
 import os
 import glob
 import pandas as pd
